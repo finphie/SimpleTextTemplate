dd88b325364b5109e5dfa243d934876228229821
finphie <19237170+finphie@users.noreply.github.com>
2021-03-14 18:47:39 +0900

    サンプルプロジェクト実装

---
+++ Source/SimpleTextTemplate.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+using SimpleTextTemplate;
+
+var bufferWriter = new ArrayBufferWriter<byte>();
+var symbols = new Dictionary<string, byte[]>
+{
+    { "Identifier", Encoding.UTF8.GetBytes("Hello, World!") }
+};
+
+TemplateEx.GeneratePageTemplate(bufferWriter, Context.Create(symbols));
+
+Console.WriteLine(Encoding.UTF8.GetString(bufferWriter.WrittenSpan));
