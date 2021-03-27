1b4c4ccab19132348ae35916f06a0d8cbb79a9fa
finphie <19237170+finphie@users.noreply.github.com>
2021-03-28 07:05:10 +0900

    サンプルプロジェクトでもArrayPoolBufferWriterを利用するように変更

--- Source/SimpleTextTemplate.Sample/Program.cs
+++ Source/SimpleTextTemplate.Sample/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Buffers;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Toolkit.HighPerformance.Buffers;
 using SimpleTextTemplate;
 using SimpleTextTemplate.Contexts;
 
@@ -12,7 +12,7 @@ var symbols = new Dictionary<string, byte[]>
     { "Identifier", Encoding.UTF8.GetBytes("Hello, World!") }
 };
 
-var bufferWriter = new ArrayBufferWriter<byte>();
+using var bufferWriter = new ArrayPoolBufferWriter<byte>();
 ZTemplate.GeneratePageTemplate(bufferWriter, Context.Create(symbols));
 
 Console.WriteLine(Encoding.UTF8.GetString(bufferWriter.WrittenSpan));
