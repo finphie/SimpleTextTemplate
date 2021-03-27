6bf361913f0016b1e93053a0eda99189170f79ab
finphie <19237170+finphie@users.noreply.github.com>
2021-03-27 12:03:05 +0900

    [CI] Update dotfiles (#10)
    * Update dotfiles
    
    * Update SimpleTextTemplate.Generator.csproj
    
    * フォーマット修正
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- Source/SimpleTextTemplate.Sample/Program.cs
+++ Source/SimpleTextTemplate.Sample/Program.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Text;
 using SimpleTextTemplate;
 
-var bufferWriter = new ArrayBufferWriter<byte>();
+#pragma warning disable SA1516 // Elements should be separated by blank line
+
 var symbols = new Dictionary<string, byte[]>
 {
     { "Identifier", Encoding.UTF8.GetBytes("Hello, World!") }
 };
 
+var bufferWriter = new ArrayBufferWriter<byte>();
 TemplateEx.GeneratePageTemplate(bufferWriter, Context.Create(symbols));
 
 Console.WriteLine(Encoding.UTF8.GetString(bufferWriter.WrittenSpan));
+
+#pragma warning restore SA1516 // Elements should be separated by blank line
