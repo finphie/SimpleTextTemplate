166ae38a9cb3b92e1be987497cb9bd9e2f6ed4ed
finphie <19237170+finphie@users.noreply.github.com>
2021-03-27 20:04:01 +0900

    ベンチマークプロジェクトを追加

---
+++ Source/SimpleTextTemplate.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace SimpleTextTemplate.Benchmarks
+{
+    /// <summary>
+    /// ベンチマーク
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+            => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
---
+++ Source/SimpleTextTemplate.Benchmarks/RenderBenchmark.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Microsoft.Toolkit.HighPerformance.Buffers;
+using SimpleTextTemplate.Abstractions;
+using SimpleTextTemplate.Contexts;
+using ScribanTemplate = Scriban.Template;
+
+namespace SimpleTextTemplate.Benchmarks
+{
+    [ShortRunJob(RuntimeMoniker.NetCoreApp50)]
+    [MemoryDiagnoser]
+    public class RenderBenchmark
+    {
+        IContext? _context;
+        Dictionary<string, string>? _model;
+
+        Template _template;
+        ScribanTemplate? _scribanTemplate;
+        ScribanTemplate? _scribanLiquidTemplate;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var source = File.ReadAllBytes("Templates/Page.html");
+            var sourceString = Encoding.UTF8.GetString(source);
+
+            _template = Template.Parse(source);
+            _context = Context.Create(new Dictionary<string, byte[]>
+            {
+                { "Identifier", Encoding.UTF8.GetBytes("Hello, World!") }
+            });
+
+            _scribanTemplate = ScribanTemplate.Parse(sourceString);
+            _scribanLiquidTemplate = ScribanTemplate.ParseLiquid(sourceString);
+            _model = new()
+            {
+                { "Identifier", "Hello, World!" }
+            };
+        }
+
+        [Benchmark]
+        public string SimpleTextTemplate()
+        {
+            using var bufferWriter = new ArrayPoolBufferWriter<byte>();
+            _template.RenderTo(bufferWriter, _context!);
+            return Encoding.UTF8.GetString(bufferWriter.WrittenSpan);
+        }
+
+        [Benchmark(Baseline = true)]
+        public string ZSimpleTextTemplate()
+        {
+            using var bufferWriter = new ArrayPoolBufferWriter<byte>();
+            TemplateEx.GeneratePageTemplate(bufferWriter, _context);
+            return Encoding.UTF8.GetString(bufferWriter.WrittenSpan);
+        }
+
+        [Benchmark]
+        public byte[] ZSimpleTextTemplateUtf8()
+        {
+            using var bufferWriter = new ArrayPoolBufferWriter<byte>();
+            TemplateEx.GeneratePageTemplate(bufferWriter, _context);
+            return bufferWriter.WrittenSpan.ToArray();
+        }
+
+        [Benchmark]
+        public string Scriban() => _scribanTemplate!.Render(_model);
+
+        [Benchmark]
+        public string ScribanLiquid() => _scribanLiquidTemplate!.Render(_model);
+    }
+}
