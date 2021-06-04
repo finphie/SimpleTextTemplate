1ea17977c087445b535d9b8009184f1ca43ea37a
finphie <19237170+finphie@users.noreply.github.com>
2021-06-05 05:37:08 +0900

    ジェネレーターのテストを追加

---
+++ Tests/SimpleTextTemplate.Generator.Tests/GeneratorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using FluentAssertions;
+using Microsoft.Toolkit.HighPerformance.Buffers;
+using SimpleTextTemplate.Contexts;
+using Utf8Utility;
+using Xunit;
+
+namespace SimpleTextTemplate.Generator.Tests
+{
+    public sealed class GeneratorTest
+    {
+        [Fact]
+        public void GeneratePageTemplateTest()
+        {
+            var key = (Utf8String)"Identifier";
+            var message = (Utf8String)"Hello, World!";
+
+            var symbols = new Utf8StringDictionary<Utf8String>();
+            symbols.Add(key, message);
+
+            using var bufferWriter = new ArrayPoolBufferWriter<byte>();
+            ZTemplate.GeneratePageTemplate(bufferWriter, Context.Create(symbols));
+
+            var expected = @$"<html>
+
+<body>
+  {message}
+</body>
+
+</html>";
+            bufferWriter.WrittenSpan.ToArray().Should().Equal(Encoding.UTF8.GetBytes(expected));
+        }
+    }
+}
