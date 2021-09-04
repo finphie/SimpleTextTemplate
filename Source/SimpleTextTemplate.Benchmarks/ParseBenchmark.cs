5e0abcd353cce76b50c5bb02c1d8fa8c693c12a5
finphie <19237170+finphie@users.noreply.github.com>
2021-09-05 06:33:59 +0900

    テンプレート解析ベンチマークを追加

---
+++ Source/SimpleTextTemplate.Benchmarks/ParseBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using ScribanTemplate = Scriban.Template;
+
+namespace SimpleTextTemplate.Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+[MemoryDiagnoser]
+public class ParseBenchmark
+{
+    byte[]? _utf8Source;
+    string? _utf16Source;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _utf8Source = File.ReadAllBytes("Templates/Page.html");
+        _utf16Source = Encoding.UTF8.GetString(_utf8Source);
+    }
+
+    [Benchmark(Baseline = true)]
+    public Template SimpleTextTemplate() => Template.Parse(_utf8Source!);
+
+    [Benchmark]
+    public ScribanTemplate Scriban() => ScribanTemplate.Parse(_utf16Source);
+
+    [Benchmark]
+    public ScribanTemplate ScribanLiquid() => ScribanTemplate.ParseLiquid(_utf16Source);
+}
