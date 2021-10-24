115ce77473f79df2eaf0b87c4c626b111b227818
finphie <19237170+finphie@users.noreply.github.com>
2021-10-24 13:01:36 +0900

    トップレベルステートメント

--- Source/SimpleTextTemplate.Benchmarks/Program.cs
+++ Source/SimpleTextTemplate.Benchmarks/Program.cs
@@ -1,12 +1,3 @@
 ﻿using BenchmarkDotNet.Running;
 
-namespace SimpleTextTemplate.Benchmarks;
-
-/// <summary>
-/// ベンチマーク
-/// </summary>
-class Program
-{
-    static void Main(string[] args)
-        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-}
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
