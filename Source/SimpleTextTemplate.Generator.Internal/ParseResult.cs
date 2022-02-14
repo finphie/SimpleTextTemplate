1365e6f906c956e6525f553422af28ddd762ff28
finphie <19237170+finphie@users.noreply.github.com>
2022-02-14 15:41:05 +0900

    Incremental Generator

---
+++ Source/SimpleTextTemplate.Generator.Internal/ParseResult.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleTextTemplate.Generator;
+
+/// <summary>
+/// 解析結果
+/// </summary>
+public enum ParseResult
+{
+    /// <summary>
+    /// なし
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// 成功
+    /// </summary>
+    Success,
+
+    /// <summary>
+    /// 無効な識別子
+    /// </summary>
+    InvalidIdentifier
+}
---
+++ Source/SimpleTextTemplate.Generator.Internal/TemplateGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace SimpleTextTemplate.Generator;
+
+/// <summary>
+/// テンプレートを生成するソースジェネレーターです。
+/// </summary>
+[Generator(LanguageNames.CSharp)]
+public sealed class TemplateGenerator : IIncrementalGenerator
+{
+    const string TemplateAttributeName = "global::SimpleTextTemplate.TemplateAttribute";
+    const string TemplateFileAttributeName = "global::SimpleTextTemplate.TemplateFileAttribute";
+
+    static readonly DiagnosticDescriptor STT1000Rule = new(
+        "STT1000",
+        "テンプレート解析時にエラーが発生しました。",
+        "テンプレート解析時に未知のエラーが発生しました。",
+        "Generator",
+        DiagnosticSeverity.Error,
+        true);
+
+    static readonly DiagnosticDescriptor STT1001Rule = new(
+        "STT1001",
+        "識別子に無効な文字が含まれています。",
+        "テンプレート解析時にエラーが発生しました。識別子に無効な文字が含まれています。",
+        "Generator",
+        DiagnosticSeverity.Error,
+        true);
+
+    /// <inheritdoc/>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var methodSymbols = context.SyntaxProvider
+            .CreateSyntaxProvider(
+                static (node, _) => node is MethodDeclarationSyntax { AttributeLists.Count: > 0 },
+                static (context, token) => (IMethodSymbol)context.SemanticModel.GetDeclaredSymbol(context.Node, token)!);
+
+        var methodSymbolsWithAttributeData = methodSymbols
+            .Select(static (symbol, token) =>
+            {
+                foreach (var attribute in symbol.GetAttributes())
+                {
+                    token.ThrowIfCancellationRequested();
+
+                    var fullName = attribute.AttributeClass?.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+                    if (fullName is not TemplateAttributeName and not TemplateFileAttributeName)
+                    {
+                        continue;
+                    }
+
+                    var source = attribute.ConstructorArguments[0].Value as string;
+                    return (Symbol: symbol, FullName: fullName, Source: source);
+                }
+
+                return default;
+            })
+            .Where(x => !string.IsNullOrEmpty(x.Source));
+
+        context.RegisterSourceOutput(methodSymbolsWithAttributeData, static (context, symbol) =>
+        {
+            var template = new SourceCodeTemplate(symbol.Symbol, symbol.Source!);
+            var result = template.TryParse();
+
+            if (result == ParseResult.None)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(STT1000Rule, null));
+                return;
+            }
+
+            if (result == ParseResult.InvalidIdentifier)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(STT1001Rule, null));
+                return;
+            }
+
+            var fileName = template.ClassName + "." + template.MethodName + ".Generated.cs";
+            context.AddSource(fileName, SourceText.From(template.TransformText(), Encoding.UTF8));
+        });
+    }
+}
