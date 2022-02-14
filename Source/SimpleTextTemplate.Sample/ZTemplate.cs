5260868aff29eb6ec96f6066d766b4ba6878ea32
finphie <19237170+finphie@users.noreply.github.com>
2022-02-14 20:41:41 +0900

    サンプル

---
+++ Source/SimpleTextTemplate.Sample/ZTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System.Buffers;
+
+namespace SimpleTextTemplate.Sample;
+
+/// <summary>
+/// テンプレートレンダリングを行うクラスです。
+/// </summary>
+static partial class ZTemplate
+{
+    [Template("{{ Identifier }}")]
+    public static partial void Render(IBufferWriter<byte> bufferWriter, SampleContext context);
+
+    [TemplateFile("Templates/Pages.html")]
+    public static partial void RenderFile(IBufferWriter<byte> bufferWriter, SampleContext context);
+}
