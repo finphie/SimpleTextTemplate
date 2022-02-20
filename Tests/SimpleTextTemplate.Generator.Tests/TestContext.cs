da7ab04a195bdf0b0f6e379672c3b48667514aff
finphie <19237170+finphie@users.noreply.github.com>
2022-02-20 19:11:18 +0900

    テスト修正

---
+++ Tests/SimpleTextTemplate.Generator.Tests/TestContext.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleTextTemplate.Generator.Tests;
+
+/// <summary>
+/// コンテキスト
+/// </summary>
+/// <param name="Identifier">識別子</param>
+record struct TestContext(byte[] Identifier);
---
+++ Tests/SimpleTextTemplate.Generator.Tests/ZTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System.Buffers;
+
+namespace SimpleTextTemplate.Generator.Tests;
+
+/// <summary>
+/// テンプレートレンダリングを行うクラスです。
+/// </summary>
+static partial class ZTemplate
+{
+    [Template("{{ Identifier }}")]
+    public static partial void Render(IBufferWriter<byte> bufferWriter, TestContext context);
+
+    [TemplateFile("Templates/Test.txt")]
+    public static partial void FileRender(IBufferWriter<byte> bufferWriter, TestContext context);
+}
