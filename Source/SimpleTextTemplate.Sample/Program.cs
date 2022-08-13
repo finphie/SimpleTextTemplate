222475d37bd3f80c26d22ce32efda1a5c520d092
finphie <19237170+finphie@users.noreply.github.com>
2022-08-14 08:20:46 +0900

    不要な抑制を削除

--- Source/SimpleTextTemplate.Sample/Program.cs
+++ Source/SimpleTextTemplate.Sample/Program.cs
@@ -2,13 +2,9 @@
 using CommunityToolkit.HighPerformance.Buffers;
 using SimpleTextTemplate.Sample;
 
-#pragma warning disable SA1516 // Elements should be separated by blank line
-
 var context = new SampleContext(Encoding.UTF8.GetBytes("Hello, World!"));
 
 using var bufferWriter = new ArrayPoolBufferWriter<byte>();
 ZTemplate.Render(bufferWriter, context);
 
 Console.WriteLine(Encoding.UTF8.GetString(bufferWriter.WrittenSpan));
-
-#pragma warning restore SA1516 // Elements should be separated by blank line
