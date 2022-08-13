0561b98ef6b20b225d56ba6a6b59731b9c388208
finphie <19237170+finphie@users.noreply.github.com>
2022-08-14 08:29:35 +0900

    Update Program.cs

--- Source/SimpleTextTemplate.Sample/Program.cs
+++ Source/SimpleTextTemplate.Sample/Program.cs
@@ -2,7 +2,7 @@
 using CommunityToolkit.HighPerformance.Buffers;
 using SimpleTextTemplate.Sample;
 
-var context = new SampleContext(Encoding.UTF8.GetBytes("Hello, World!"));
+var context = new SampleContext("Hello, World!"u8.ToArray());
 
 using var bufferWriter = new ArrayPoolBufferWriter<byte>();
 ZTemplate.Render(bufferWriter, context);
