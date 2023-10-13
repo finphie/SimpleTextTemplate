13516f2189dca79d27670a06b533947d0112287e
finphie <19237170+finphie@users.noreply.github.com>
2023-10-14 08:30:26 +0900

    警告抑制

--- Source/SimpleTextTemplate.Sample/SampleContext.cs
+++ Source/SimpleTextTemplate.Sample/SampleContext.cs
@@ -1,7 +1,10 @@
-﻿namespace SimpleTextTemplate.Sample;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleTextTemplate.Sample;
 
 /// <summary>
 /// コンテキスト
 /// </summary>
 /// <param name="Identifier">識別子</param>
+[SuppressMessage("Performance", "CA1819:プロパティは配列を返すことはできません", Justification = "配列である必要があるため。")]
 readonly record struct SampleContext(byte[] Identifier);
