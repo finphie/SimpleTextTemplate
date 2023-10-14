929ea7c0f4955b74cf61cea3ac75cb7fe48c068b
finphie <19237170+finphie@users.noreply.github.com>
2023-10-14 16:04:15 +0900

    リファクタリング

--- Source/SimpleTextTemplate.Generator.Internal/TemplateGenerator.cs
+++ Source/SimpleTextTemplate.Generator.Internal/TemplateGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 using SimpleTextTemplate.Generator.Extensions;
 
 namespace SimpleTextTemplate.Generator;
@@ -7,14 +9,37 @@ namespace SimpleTextTemplate.Generator;
 /// 属性引数からテンプレート文字列を取得して、ソースコードを生成します。
 /// </summary>
 [Generator(LanguageNames.CSharp)]
-public sealed class TemplateGenerator : IncrementalGenerator
+public sealed class TemplateGenerator : IIncrementalGenerator
 {
     const string TemplateAttributeName = $"global::{nameof(SimpleTextTemplate)}.TemplateAttribute";
 
     /// <inheritdoc/>
-    protected override string GetGeneratorName() => nameof(TemplateGenerator);
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var methods = context.GetMethodSymbolsWithAttributeArgumentProvider(TemplateAttributeName);
 
-    /// <inheritdoc/>
-    protected override IncrementalValuesProvider<MethodSymbolWithArgument> GetProvider(IncrementalGeneratorInitializationContext context)
-        => context.GetMethodSymbolsWithAttributeArgumentProvider(TemplateAttributeName);
+        context.RegisterSourceOutput(methods, (context, method) =>
+        {
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            var source = Encoding.UTF8.GetBytes(method.Argument);
+            var template = new SourceCodeTemplate(method.Symbol, source);
+            var result = template.TryParse();
+
+            if (result == ParseResult.InvalidIdentifier)
+            {
+                context.ReportDiagnostic(DiagnosticDescriptors.InvalidIdentifierError, method.Symbol);
+                return;
+            }
+
+            if (result != ParseResult.Success)
+            {
+                context.ReportDiagnostic(DiagnosticDescriptors.UnknownError, method.Symbol);
+                return;
+            }
+
+            var fileName = $"__{nameof(TemplateGenerator)}.{template.ClassName}.{template.MethodName}.Generated.cs";
+            context.AddSource(fileName, SourceText.From(template.TransformText(), Encoding.UTF8));
+        });
+    }
 }
