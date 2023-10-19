d54a0170e838bc7c6c1248307b228903f3e78c2d
finphie <19237170+finphie@users.noreply.github.com>
2023-10-19 22:27:35 +0900

    Update TemplateReaderReadTest.cs

--- Tests/SimpleTextTemplate.Tests/TemplateReaderReadTest.cs
+++ Tests/SimpleTextTemplate.Tests/TemplateReaderReadTest.cs
@@ -8,7 +8,7 @@ namespace SimpleTextTemplate.Tests;
 public sealed class TemplateReaderReadTest
 {
     [Fact]
-    public void テンプレート文字列_文字列の範囲を返す()
+    public void 文字列_文字列の範囲を返す()
     {
         Execute("{"u8, (Raw, "{", 1));
         Execute("a"u8, (Raw, "a", 1));
@@ -26,22 +26,44 @@ public sealed class TemplateReaderReadTest
     }
 
     [Fact]
-    public void テンプレート文字列_識別子_テンプレート文字列_テンプレート文字列または識別子の範囲を返す()
+    public void 文字列_識別子_文字列_文字列または識別子の範囲を返す()
     {
         Execute("z{{A}}z"u8, (Raw, "z", 1), (Identifier, "A", 6), (Raw, "z", 7));
         Execute("z{{ A }}z"u8, (Raw, "z", 1), (Identifier, "A", 8), (Raw, "z", 9));
     }
 
     [Fact]
-    public void 識別子_テンプレート文字列_識別子_テンプレート文字列または識別子の範囲を返す()
+    public void 識別子_文字列_識別子_文字列または識別子の範囲を返す()
     {
         Execute("{{ AAA }}z{{ BBB }}"u8, (Identifier, "AAA", 9), (Raw, "z", 10), (Identifier, "BBB", 19));
         Execute("{{ A }}123{{ B }}"u8, (Identifier, "A", 7), (Raw, "123", 10), (Identifier, "B", 17));
     }
 
     [Fact]
-    public void テンプレート文字列_識別子_テンプレート文字列_識別子_テンプレート文字列_テンプレート文字列または識別子の範囲を返す()
+    public void 文字列_識別子_文字列_識別子_文字列_文字列または識別子の範囲を返す()
         => Execute("x{{ A }}123{{ B }}x"u8, (Raw, "x", 1), (Identifier, "A", 8), (Raw, "123", 11), (Identifier, "B", 18), (Raw, "x", 19));
+
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("{{", 2)]
+    [InlineData("{{ ", 3)]
+    [InlineData("{{ }", 3)]
+    public void 識別子終了タグなし_TemplateException(string input, int position)
+    {
+        // nuint型とint型との比較の際、例外が発生する。
+        // そのため、int型をあらかじめUIntPtr型でキャストしておく。
+        var positionPtr = (UIntPtr)position;
+
+        var utf8Input = Encoding.UTF8.GetBytes(input);
+        utf8Input.Invoking(static array =>
+        {
+            var reader = new TemplateReader(array);
+            reader.Read(out _);
+        })
+        .Should()
+        .Throw<TemplateException>()
+        .Where(x => x.Position == positionPtr);
+    }
 
     static void Execute(ReadOnlySpan<byte> buffer, params (BlockType Type, string Value, nuint Consumed)[] blocks)
     {
