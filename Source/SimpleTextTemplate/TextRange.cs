7f47c54d214ee4448e2eea251978f391ef605a05
finphie <19237170+finphie@users.noreply.github.com>
2023-10-19 21:35:23 +0900

    record

--- Source/SimpleTextTemplate/TextRange.cs
+++ Source/SimpleTextTemplate/TextRange.cs
@@ -1,45 +1,17 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace SimpleTextTemplate;
+﻿namespace SimpleTextTemplate;
 
 /// <summary>
 /// 範囲を表す構造体です。
 /// </summary>
-[SuppressMessage("Performance", "CA1815:equals および operator equals を値型でオーバーライドします", Justification = "不要なため。")]
-#if !IsGenerator
-public
-#endif
-readonly struct TextRange
+/// <param name="Start">開始位置</param>
+/// <param name="End">終了位置</param>
+public readonly record struct TextRange(int Start, int End)
 {
-    /// <summary>
-    /// <see cref="TextRange"/>構造体の新しいインスタンスを初期化します。
-    /// </summary>
-    /// <param name="start">開始位置</param>
-    /// <param name="end">終了位置</param>
-    public TextRange(int start, int end)
-        => (Start, End) = (start, end);
-
-    /// <summary>
-    /// 開始位置を取得します。
-    /// </summary>
-    /// <value>
-    /// 開始位置
-    /// </value>
-    public int Start { get; }
-
-    /// <summary>
-    /// 終了位置を取得します。
-    /// </summary>
-    /// <value>
-    /// 終了位置
-    /// </value>
-    public int End { get; }
-
     /// <summary>
     /// 長さを取得します。
     /// </summary>
     /// <value>
-    /// 長さ
+    /// <see cref="End"/>-<see cref="Start"/>
     /// </value>
     public int Length => End - Start;
 }
