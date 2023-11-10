13524aebc8b2aa3946f7d2b5d034238fec78485a
finphie <19237170+finphie@users.noreply.github.com>
2023-11-11 07:58:28 +0900

    サンプルプロジェクト修正

--- Source/SimpleTextTemplate.Sample/Program.cs
+++ Source/SimpleTextTemplate.Sample/Program.cs
@@ -1,10 +1,23 @@
-﻿using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using CommunityToolkit.HighPerformance.Buffers;
-using SimpleTextTemplate.Sample;
+using SimpleTextTemplate;
 
-var context = new SampleContext("Hello, World!"u8.ToArray());
+var context = new SampleContext("Hello, World");
 
-using var bufferWriter = new ArrayPoolBufferWriter<byte>();
-ZTemplate.Render(bufferWriter, context);
+var bufferWriter = new ArrayPoolBufferWriter<byte>();
+var template = new TemplateWriter<ArrayPoolBufferWriter<byte>>(ref bufferWriter);
+
+template.Write("{{ Identifier }}!!!", context);
+template.Dispose();
 
 Console.WriteLine(Encoding.UTF8.GetString(bufferWriter.WrittenSpan));
+
+bufferWriter.Dispose();
+
+/// <summary>
+/// コンテキスト
+/// </summary>
+/// <param name="Identifier">識別子</param>
+[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "サンプルプロジェクト")]
+readonly record struct SampleContext(string Identifier);
