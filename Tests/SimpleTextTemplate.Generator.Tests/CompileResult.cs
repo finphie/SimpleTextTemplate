3551a174818a300cf4d70f828e5a57cd903e9f41
finphie <19237170+finphie@users.noreply.github.com>
2024-05-03 23:54:14 +0900

    GeneratorRunner

---
+++ Tests/SimpleTextTemplate.Generator.Tests/CompileResult.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SimpleTextTemplate.Generator.Tests;
+
+/// <summary>
+/// コンパイル結果を表すクラスです。
+/// </summary>
+/// <param name="Compilation">コンパイル情報</param>
+/// <param name="Diagnostics">診断情報</param>
+sealed record CompileResult(Compilation Compilation, IReadOnlyList<Diagnostic> Diagnostics);
