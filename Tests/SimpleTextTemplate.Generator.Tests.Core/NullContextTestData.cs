89d498cd65709a52de712ad1b30b88309e7702a1
finphie <19237170+finphie@users.noreply.github.com>
2024-05-11 13:45:25 +0900

    識別子null実行テスト

--- Tests/SimpleTextTemplate.Generator.Tests.Core/NullContextTestData.cs
+++ Tests/SimpleTextTemplate.Generator.Tests.Core/NullContextTestData.cs
@@ -8,4 +8,16 @@ public sealed record NullContextTestData
 
     public static readonly string? NullStringStaticField;
     public static readonly object? NullObjectStaticField;
+    public static readonly string EmptyStringStaticField = string.Empty;
+
+    public readonly string? NullStringField;
+    public readonly object? NullObjectField;
+
+    public static string? NullStringStaticProperty => null;
+
+    public static object? NullObjectStaticProperty => null;
+
+    public static string? NullStringProperty => null;
+
+    public static object? NullObjectProperty => null;
 }
