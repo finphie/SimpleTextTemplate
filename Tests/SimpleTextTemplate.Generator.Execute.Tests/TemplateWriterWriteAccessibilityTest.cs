95195fba425d134545e3f5b236b00cfd194b23e7
finphie <19237170+finphie@users.noreply.github.com>
2024-05-11 13:50:48 +0900

    リファクタリング

--- Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateWriterWriteAccessibilityTest.cs
+++ Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateWriterWriteAccessibilityTest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Buffers;
-using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using FluentAssertions;
 using Xunit;
@@ -135,24 +134,4 @@ public sealed class TemplateWriterWriteAccessibilityTest
             .Should()
             .Be("_ProtectedInternalProperty");
     }
-}
-
-[SuppressMessage("Design", "CA1051:参照可能なインスタンス フィールドを宣言しません", Justification = "テストのため")]
-[SuppressMessage("Performance", "CA1819:プロパティは配列を返すことはできません", Justification = "テストのため")]
-[SuppressMessage("Performance", "CA1822:メンバーを static に設定します", Justification = "テストのため")]
-record AccessibilityTestData
-{
-    internal static byte[] _internalStaticField = "InternalStaticField"u8.ToArray();
-    internal byte[] _internalField = "InternalField"u8.ToArray();
-
-    protected internal static readonly byte[] ProtectedInternalStaticField = "_ProtectedInternalStaticField"u8.ToArray();
-    protected internal readonly byte[] ProtectedInternalField = "_ProtectedInternalField"u8.ToArray();
-
-    internal static byte[] InternalStaticProperty => "_InternalStaticProperty"u8.ToArray();
-
-    internal byte[] InternalProperty => "_InternalProperty"u8.ToArray();
-
-    protected internal static byte[] ProtectedInternalStaticProperty => "_ProtectedInternalStaticProperty"u8.ToArray();
-
-    protected internal byte[] ProtectedInternalProperty => "_ProtectedInternalProperty"u8.ToArray();
 }
