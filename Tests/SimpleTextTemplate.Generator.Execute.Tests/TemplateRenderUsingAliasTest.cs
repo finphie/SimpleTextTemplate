e67873eafdddc7044b801e01408d7b38962c25bf
finphie <19237170+finphie@users.noreply.github.com>
2024-11-16 21:57:53 +0900

    Create TemplateRenderUsingAliasTest.cs

---
+++ Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateRenderUsingAliasTest.cs
@@ -0,0 +1,71 @@
+﻿using System.Buffers;
+using System.Text;
+using FluentAssertions;
+using SimpleTextTemplate.Generator.Tests.Core;
+using Xunit;
+using TemplateRenderer = SimpleTextTemplate.Template;
+
+namespace SimpleTextTemplate.Generator.Execute.Tests;
+
+public sealed class TemplateRenderUsingAliasTest
+{
+    [Fact]
+    public void 静的フィールド()
+    {
+        var context = new ByteArrayContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesStaticField }}", in context);
+        writer.Dispose();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .Should()
+            .Be("_BytesStaticField");
+    }
+
+    [Fact]
+    public void フィールド()
+    {
+        var context = new ByteArrayContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesField }}", in context);
+        writer.Dispose();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .Should()
+            .Be("_BytesField");
+    }
+
+    [Fact]
+    public void 静的プロパティ()
+    {
+        var context = new ByteArrayContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesStaticProperty }}", in context);
+        writer.Dispose();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .Should()
+            .Be("_BytesStaticProperty");
+    }
+
+    [Fact]
+    public void プロパティ()
+    {
+        var context = new ByteArrayContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesProperty }}", in context);
+        writer.Dispose();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .Should()
+            .Be("_BytesProperty");
+    }
+}
