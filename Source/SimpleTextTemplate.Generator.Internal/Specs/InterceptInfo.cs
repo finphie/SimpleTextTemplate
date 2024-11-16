f444e752d020bef3de34a0bb3e95f377ab7a97aa
finphie <19237170+finphie@users.noreply.github.com>
2024-11-16 21:46:02 +0900

    Update InterceptInfo.cs

--- Source/SimpleTextTemplate.Generator.Internal/Specs/InterceptInfo.cs
+++ Source/SimpleTextTemplate.Generator.Internal/Specs/InterceptInfo.cs
@@ -1,10 +1,14 @@
-﻿namespace SimpleTextTemplate.Generator.Specs;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace SimpleTextTemplate.Generator.Specs;
 
 /// <summary>
 /// インターセプターに必要な情報を格納するクラスです。
 /// </summary>
 /// <param name="LocationInfo">InterceptsLocation属性の情報</param>
 /// <param name="WriteInfoList">Writeメソッドの種類</param>
-/// <param name="WriterTypeName">TemplateWriterの型名</param>
-/// <param name="ContextTypeName">コンテキストの型名</param>
-sealed record InterceptInfo(InterceptsLocationInfo LocationInfo, IReadOnlyList<TemplateWriterWriteInfo> WriteInfoList, string WriterTypeName, string? ContextTypeName);
+/// <param name="MethodSymbol">メソッドシンボル</param>
+#pragma warning disable RSEXPERIMENTAL002 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
+sealed record InterceptInfo(InterceptableLocation LocationInfo, IReadOnlyList<TemplateWriterWriteInfo> WriteInfoList, IMethodSymbol MethodSymbol);
+#pragma warning restore RSEXPERIMENTAL002 // 種類は、評価の目的でのみ提供されています。将来の更新で変更または削除されることがあります。続行するには、この診断を非表示にします。
