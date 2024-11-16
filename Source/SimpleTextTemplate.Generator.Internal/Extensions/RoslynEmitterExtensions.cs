0299a9d2c09a8019ec32c4f56778969ac1893624
finphie <19237170+finphie@users.noreply.github.com>
2024-11-16 22:19:21 +0900

    Create RoslynEmitterExtensions.cs

---
+++ Source/SimpleTextTemplate.Generator.Internal/Extensions/RoslynEmitterExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SimpleTextTemplate.Generator.Extensions;
+
+/// <summary>
+/// Roslynのコード生成に関する拡張メソッド集です。
+/// </summary>
+static class RoslynEmitterExtensions
+{
+    /// <summary>
+    /// パラメーターのテキストを取得します。
+    /// </summary>
+    /// <param name="symbol">パラメーターシンボル</param>
+    /// <returns>パラメーターのテキストを返します。</returns>
+    public static string GetParameterText(this IParameterSymbol symbol)
+    {
+        var modifiers = symbol.RefKind.GetParameterModifiers();
+        var typeName = symbol.Type.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+        var name = symbol.Name;
+        var defaultValue = symbol.HasExplicitDefaultValue ? $"= {GetDefaultValue(symbol)}" : string.Empty;
+
+        var parameters = new[] { modifiers, typeName, name, defaultValue }
+            .Where(static x => !string.IsNullOrEmpty(x));
+        return string.Join(" ", parameters);
+    }
+
+    static string GetParameterModifiers(this RefKind refKind)
+    {
+        return refKind switch
+        {
+            RefKind.None => string.Empty,
+            RefKind.Ref => "ref",
+            RefKind.Out => "out",
+            RefKind.In => "in",
+            RefKind.RefReadOnlyParameter => "ref readonly",
+            _ => throw new ArgumentOutOfRangeException(nameof(refKind), refKind, "Unexpected RefKind value.")
+        };
+    }
+
+    static string GetDefaultValue(this IParameterSymbol symbol)
+    {
+        return !symbol.HasExplicitDefaultValue
+            ? string.Empty
+            : symbol.ExplicitDefaultValue switch
+            {
+                null => "null",
+                _ => throw new InvalidOperationException("サポート対象外のデフォルト値です。")
+            };
+    }
+}
