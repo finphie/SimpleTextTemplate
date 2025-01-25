c45a246ceaacd1aec8a14e37a7d221a26cd1dc2e
finphie <19237170+finphie@users.noreply.github.com>
2025-01-25 13:02:27 +0900

    Update Program.cs

--- Source/SimpleTextTemplate.Benchmarks/Program.cs
+++ Source/SimpleTextTemplate.Benchmarks/Program.cs
@@ -5,8 +5,8 @@ using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 var config = DefaultConfig.Instance
-    .AddJob(Job.Default.WithId("No PGO").WithRuntime(CoreRuntime.Core80).WithEnvironmentVariable("DOTNET_TieredPGO", "0"))
-    .AddJob(Job.Default.WithId("PGO").WithRuntime(CoreRuntime.Core80))
+    .AddJob(Job.Default.WithId("No PGO").WithRuntime(CoreRuntime.Core90).WithEnvironmentVariable("DOTNET_TieredPGO", "0"))
+    .AddJob(Job.Default.WithId("PGO").WithRuntime(CoreRuntime.Core90))
     .AddDiagnoser(MemoryDiagnoser.Default)
     .HideColumns("StdDev", "Median", "RatioSD", "Alloc Ratio", "EnvironmentVariables");
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
