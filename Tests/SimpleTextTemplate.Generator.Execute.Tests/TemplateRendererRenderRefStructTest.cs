616b2470001417e3ebbf65bd5ce08c9a57f84fb9
finphie <19237170+finphie@users.noreply.github.com>
2025-01-26 16:25:47 +0900

    ref struct関連のテストを追加 (#270)

---
+++ Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateRendererRenderRefStructTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Buffers;
+using System.Text;
+using Shouldly;
+using SimpleTextTemplate.Generator.Tests.Core;
+using Xunit;
+
+namespace SimpleTextTemplate.Generator.Execute.Tests;
+
+public sealed class TemplateRendererRenderRefStructTest
+{
+    [Fact]
+    public void 静的フィールド()
+    {
+        var context = new RefStructTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesStaticField }}", in context);
+        writer.Flush();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .ShouldBe("_BytesStaticField");
+    }
+
+    [Fact]
+    public void フィールド()
+    {
+        var context = new RefStructTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesField }}", in context);
+        writer.Flush();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .ShouldBe("_BytesField");
+    }
+
+    [Fact]
+    public void ReadOnlySpanフィールド()
+    {
+        var context = new RefStructTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesSpanField }}", in context);
+        writer.Flush();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .ShouldBe("_BytesSpanField");
+    }
+
+    [Fact]
+    public void 静的プロパティ()
+    {
+        var context = new RefStructTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesStaticProperty }}", in context);
+        writer.Flush();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .ShouldBe("_BytesStaticProperty");
+    }
+
+    [Fact]
+    public void プロパティ()
+    {
+        var context = new RefStructTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        var writer = TemplateWriter.Create(bufferWriter);
+        TemplateRenderer.Render(ref writer, "{{ BytesProperty }}", in context);
+        writer.Flush();
+
+        Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
+            .ShouldBe("_BytesProperty");
+    }
+}
---
+++ Tests/SimpleTextTemplate.Generator.Tests.Core/RefStructTestData.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleTextTemplate.Generator.Tests.Core;
+
+public readonly ref struct RefStructTestData()
+{
+    public static readonly byte[] BytesStaticField = "_BytesStaticField"u8.ToArray();
+    public readonly byte[] BytesField = "_BytesField"u8.ToArray();
+    public readonly ReadOnlySpan<byte> BytesSpanField = "_BytesSpanField"u8;
+
+    public static byte[] BytesStaticProperty => "_BytesStaticProperty"u8.ToArray();
+
+    public byte[] BytesProperty => "_BytesProperty"u8.ToArray();
+}
