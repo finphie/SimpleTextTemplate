716ec30e85c44a9782cde09e641ff5ae50bf8d6b
finphie <19237170+finphie@users.noreply.github.com>
2025-02-21 21:44:02 +0900

    Create DoubleContextTestData.cs

---
+++ Tests/SimpleTextTemplate.Generator.Tests.Core/DoubleContextTestData.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleTextTemplate.Generator.Tests.Core;
+
+public sealed record DoubleContextTestData
+{
+    public const double DoubleConstantField = 1234.567;
+
+    public static readonly double DoubleStaticField = 1234.567;
+    public readonly double DoubleField = 1234.567;
+
+    public static double DoubleStaticProperty => 1234.567;
+
+    public double DoubleProperty => 1234.567;
+}
