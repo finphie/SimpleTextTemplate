7b2a8c4d931fe00203f1774a76170a1ae1808a01
finphie <19237170+finphie@users.noreply.github.com>
2025-02-21 21:54:53 +0900

    Update DoubleContextTestData.cs

--- Tests/SimpleTextTemplate.Generator.Tests.Core/DoubleContextTestData.cs
+++ Tests/SimpleTextTemplate.Generator.Tests.Core/DoubleContextTestData.cs
@@ -4,10 +4,10 @@ public sealed record DoubleContextTestData
 {
     public const double DoubleConstantField = 1234.567;
 
-    public static readonly double DoubleStaticField = 1234.567;
-    public readonly double DoubleField = 1234.567;
+    public static readonly double DoubleStaticField = 2345.678;
+    public readonly double DoubleField = 3456.789;
 
-    public static double DoubleStaticProperty => 1234.567;
+    public static double DoubleStaticProperty => 4567.891;
 
-    public double DoubleProperty => 1234.567;
+    public double DoubleProperty => 5678.912;
 }
