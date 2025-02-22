7eec11bb54a507b4d91ab3dfb9068a488c6f15bc
finphie <19237170+finphie@users.noreply.github.com>
2025-02-22 12:41:56 +0900

    空白

--- Tests/SimpleTextTemplate.Generator.Tests.Core/StringContextTestData.cs
+++ Tests/SimpleTextTemplate.Generator.Tests.Core/StringContextTestData.cs
@@ -3,6 +3,8 @@
 public sealed record StringContextTestData
 {
     public const string StringConstantField = "_StringConstantField";
+    public const string StringConstantEmptyField = "";
+    public const string StringConstantNullField = null;
 
     public static readonly string StringStaticField = "_StringStaticField";
     public readonly string StringField = "_StringField";
