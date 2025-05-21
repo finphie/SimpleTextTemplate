6ce1b61b7d8f48b25a295ff649a74151e3a98e86
finphie <19237170+finphie@users.noreply.github.com>
2025-05-21 20:29:30 +0900

    Update TemplateRendererRenderEmptyTest.cs

--- Tests/SimpleTextTemplate.Generator.Tests/TemplateRendererRenderEmptyTest.cs
+++ Tests/SimpleTextTemplate.Generator.Tests/TemplateRendererRenderEmptyTest.cs
@@ -2,7 +2,6 @@
 using SimpleTextTemplate.Generator.Tests.Core;
 using SimpleTextTemplate.Generator.Tests.Extensions;
 using Xunit;
-using static SimpleTextTemplate.Generator.Tests.Constants;
 using static SimpleTextTemplate.Generator.Tests.GeneratorRunner;
 using static SimpleTextTemplate.Generator.Tests.SourceCode;
 
@@ -33,56 +32,6 @@ public sealed class TemplateRendererRenderEmptyTest
         diagnostics.ShouldBeEmpty();
 
         interceptInfoList.Dequeue().Methods.ShouldBeEmpty();
-        interceptInfoList.ShouldBeEmpty();
-    }
-
-    [Fact]
-    public void 静的フィールド()
-        => Test(nameof(EmptyContextTestData.EmptyStringStaticField), true);
-
-    [Fact]
-    public void フィールド()
-        => Test(nameof(EmptyContextTestData.EmptyStringField), false);
-
-    [Fact]
-    public void 静的プロパティ()
-        => Test(nameof(EmptyContextTestData.EmptyStringStaticProperty), true);
-
-    [Fact]
-    public void プロパティ()
-        => Test(nameof(EmptyContextTestData.EmptyStringProperty), false);
-
-    static void Test(string memberName, bool isStatic)
-    {
-        var templateText = $$$"""{{ {{{memberName}}} }}""";
-        var contextArgument = GetContextArgumentString<EmptyContextTestData>(memberName, isStatic);
-
-        var sourceCode = Get(templateText, nameof(EmptyContextTestData));
-        var (compilation, diagnostics) = Run(sourceCode);
-        var interceptInfoList = compilation.GetInterceptInfo();
-
-        diagnostics.ShouldBeEmpty();
-
-        var info = interceptInfoList.Dequeue();
-        var method = info.Methods.Dequeue();
-
-        method.Name.ShouldBe(Grow);
-        method.Text.Count.ShouldBe(3);
-        method.Text[0].ShouldBe("0");
-        method.Text[1].ShouldBe(Utf8GetMaxByteCount);
-        method.Text[2].ShouldBe($"{contextArgument}.Length");
-        method.Format.ShouldBeNull();
-        method.Provider.ShouldBeNull();
-
-        method = info.Methods.Dequeue();
-
-        method.Name.ShouldBe(DangerousWriteString);
-        method.Text.Count.ShouldBe(1);
-        method.Text[0].ShouldBe(contextArgument);
-        method.Format.ShouldBeNull();
-        method.Provider.ShouldBeNull();
-
-        info.Methods.ShouldBeEmpty();
         interceptInfoList.ShouldBeEmpty();
     }
 }
