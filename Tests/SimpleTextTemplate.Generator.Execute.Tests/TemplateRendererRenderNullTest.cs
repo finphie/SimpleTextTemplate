a368f9f62efc039cd83b2bcfa2b4ccf131d502fe
finphie <19237170+finphie@users.noreply.github.com>
2025-05-21 20:28:42 +0900

    null

--- Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateRendererRenderNullTest.cs
+++ Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateRendererRenderNullTest.cs
@@ -24,18 +24,67 @@ public sealed class TemplateRendererRenderNullTest
     }
 
     [Fact]
+    public void 静的フィールド_NullReferenceException()
+    {
+        var context = new NullContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullBytesStaticField }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullCharsStaticField }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullStringStaticField }}", in context);
+        });
+    }
+
+    [Fact]
     public void 静的フィールド()
     {
         var context = new NullContextTestData();
         var bufferWriter = new ArrayBufferWriter<byte>();
 
         var writer = TemplateWriter.Create(bufferWriter);
-        TemplateRenderer.Render(ref writer, "{{ NullStringStaticField }}", in context);
         TemplateRenderer.Render(ref writer, "{{ NullObjectStaticField }}", in context);
         writer.Flush();
 
         Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
             .ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void フィールド_NullReferenceException()
+    {
+        var context = new NullContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullBytesField }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullCharsField }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullStringField }}", in context);
+        });
     }
 
     [Fact]
@@ -45,12 +94,36 @@ public sealed class TemplateRendererRenderNullTest
         var bufferWriter = new ArrayBufferWriter<byte>();
 
         var writer = TemplateWriter.Create(bufferWriter);
-        TemplateRenderer.Render(ref writer, "{{ NullStringField }}", in context);
         TemplateRenderer.Render(ref writer, "{{ NullObjectField }}", in context);
         writer.Flush();
 
         Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
             .ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void 静的プロパティ_NullReferenceException()
+    {
+        var context = new NullContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullBytesStaticProperty }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullCharsStaticProperty }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullStringStaticProperty }}", in context);
+        });
     }
 
     [Fact]
@@ -60,12 +133,36 @@ public sealed class TemplateRendererRenderNullTest
         var bufferWriter = new ArrayBufferWriter<byte>();
 
         var writer = TemplateWriter.Create(bufferWriter);
-        TemplateRenderer.Render(ref writer, "{{ NullStringStaticProperty }}", in context);
         TemplateRenderer.Render(ref writer, "{{ NullObjectStaticProperty }}", in context);
         writer.Flush();
 
         Encoding.UTF8.GetString(bufferWriter.WrittenSpan)
             .ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void プロパティ_NullReferenceException()
+    {
+        var context = new NullContextTestData();
+        var bufferWriter = new ArrayBufferWriter<byte>();
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullBytesProperty }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullCharsProperty }}", in context);
+        });
+
+        Should.Throw<NullReferenceException>(() =>
+        {
+            var writer = TemplateWriter.Create(bufferWriter);
+            TemplateRenderer.Render(ref writer, "{{ NullStringProperty }}", in context);
+        });
     }
 
     [Fact]
@@ -75,7 +172,6 @@ public sealed class TemplateRendererRenderNullTest
         var bufferWriter = new ArrayBufferWriter<byte>();
 
         var writer = TemplateWriter.Create(bufferWriter);
-        TemplateRenderer.Render(ref writer, "{{ NullStringProperty }}", in context);
         TemplateRenderer.Render(ref writer, "{{ NullObjectProperty }}", in context);
         writer.Flush();
 
--- Tests/SimpleTextTemplate.Generator.Tests.Core/NullContextTestData.cs
+++ Tests/SimpleTextTemplate.Generator.Tests.Core/NullContextTestData.cs
@@ -5,15 +5,27 @@ public sealed record NullContextTestData
     public const string? NullStringConstantField = null;
     public const object? NullObjectConstantField = null;
 
+    public static readonly byte[]? NullBytesStaticField;
+    public static readonly char[]? NullCharsStaticField;
     public static readonly string? NullStringStaticField;
     public static readonly object? NullObjectStaticField;
 
+    public readonly byte[]? NullBytesField;
+    public readonly char[]? NullCharsField;
     public readonly string? NullStringField;
     public readonly object? NullObjectField;
+
+    public static byte[]? NullBytesStaticProperty => null;
+
+    public static char[]? NullCharsStaticProperty => null;
 
     public static string? NullStringStaticProperty => null;
 
     public static object? NullObjectStaticProperty => null;
+
+    public byte[]? NullBytesProperty => null;
+
+    public char[]? NullCharsProperty => null;
 
     public string? NullStringProperty => null;
 
