3afb7b0f0afa3632eed088c6c93b1b9425e28f5c
finphie <19237170+finphie@users.noreply.github.com>
2026-04-29 14:24:51 +0900

    SimpleTextTemplate.Tests.Core

--- Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateRendererRenderTest.cs
+++ Tests/SimpleTextTemplate.Generator.Execute.Tests/TemplateRendererRenderTest.cs
@@ -1,8 +1,8 @@
 ﻿using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
-using SimpleTextTemplate.Generator.Execute.Tests.Buffers;
 using SimpleTextTemplate.Generator.Tests.TestData;
 using SimpleTextTemplate.Tests.Assertions;
+using SimpleTextTemplate.Tests.Buffers;
 
 namespace SimpleTextTemplate.Generator.Execute.Tests;
 
---
+++ Tests/SimpleTextTemplate.Tests.Core/Assertions/AssertionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using TUnit.Assertions.Conditions;
+using TUnit.Assertions.Core;
+using TUnit.Assertions.Enums;
+
+namespace SimpleTextTemplate.Tests.Assertions;
+
+public static class AssertionExtensions
+{
+    extension(IAssertionSource<ReadOnlyMemory<byte>> source)
+    {
+        public IsEquivalentToAssertion<char[], char> IsUtf8SequenceEqualTo(string? expected)
+        {
+            return new IsEquivalentToAssertion<char[], char>(
+                source.Context.Map(static x => Encoding.UTF8.GetChars(x.Span.ToArray())),
+                expected?.ToCharArray() ?? [],
+                CollectionOrdering.Matching);
+        }
+    }
+}
