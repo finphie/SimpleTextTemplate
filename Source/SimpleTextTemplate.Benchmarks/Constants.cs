0431f7f894bcc28a448364fe73aa4fd5ae1e0ce5
finphie <19237170+finphie@users.noreply.github.com>
2026-04-30 18:29:36 +0900

    Update Constants.cs

--- Source/SimpleTextTemplate.Benchmarks/Constants.cs
+++ Source/SimpleTextTemplate.Benchmarks/Constants.cs
@@ -6,8 +6,8 @@ static class Constants
     public const string DescriptionSimpleTextTemplateGenerator = "SimpleTextTemplate.Generator";
     public const string DescriptionScriban = "Scriban";
     public const string DescriptionScribanLiquid = "Scriban_Liquid";
-    public const string DescriptionUtf8TryWrite = "(Utf8.TryWrite)";
-    public const string DescriptionInterpolatedStringHandler = "(InterpolatedStringHandler)";
-    public const string DescriptionCompositeFormat = "(CompositeFormat)";
-    public const string Format = "abcdef{0}abcdef";
+    public const string DescriptionUtf8TryWrite = "Utf8.TryWrite";
+    public const string DescriptionInterpolatedStringHandler = "InterpolatedStringHandler";
+    public const string DescriptionCompositeFormat = "CompositeFormat";
+    public const string Format = "{0}{0}{0}{0}{0}";
 }
